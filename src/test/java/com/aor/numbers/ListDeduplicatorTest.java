02cb57b828bd7d6db9bd4bcd480f9028728e1912
Catarina <1201338@isep.ipp.pt>
2021-11-22 12:09:52 +0000

    Using Mockito

--- src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -3,6 +3,7 @@ package com.aor.numbers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,27 +21,26 @@ public class ListDeduplicatorTest {
     @Test
     public void deduplicate() {
 
-//        class StubListDeduplicator implements GenericListDeduplicator{
-//            @Override public List<Integer> deduplicate(List<Integer> list) {
-//                return  Arrays.asList(1, 2, 4);
+//        class StubListSorter implements GenericListSorter{
+//            @Override public List<Integer> sort(List<Integer> list) {
+//                return  Arrays.asList(1, 2, 2, 4);
 //            }
 //        }
+//
 //        ListDeduplicator duplicator = new ListDeduplicator();
-//        StubListDeduplicator deduplicator = new StubListDeduplicator();
-//        int distinct = duplicator.deduplicate(list,deduplicator);
+//        StubListSorter sorter = new StubListSorter();
+//        List<Integer> distinct = duplicator.deduplicate2(list,sorter);
+//
 //
 //        Assertions.assertEquals(expected, distinct);
 
-        class StubListSorter implements GenericListSorter{
-            @Override public List<Integer> sort(List<Integer> list) {
-                return  Arrays.asList(1, 2, 2, 4);
-            }
-        }
+//Using Mockito:
+
+        GenericListSorter sorter = Mockito.mock(GenericListSorter.class);
+        Mockito.when(sorter.sort(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 2, 4));
 
         ListDeduplicator duplicator = new ListDeduplicator();
-        StubListSorter sorter = new StubListSorter();
         List<Integer> distinct = duplicator.deduplicate2(list,sorter);
-
 
         Assertions.assertEquals(expected, distinct);
     }

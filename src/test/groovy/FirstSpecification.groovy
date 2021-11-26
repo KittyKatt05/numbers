27b8819ba460caf3726a06300d53539b39be43e6
Catarina <1201338@isep.ipp.pt>
2021-11-26 15:00:08 +0000

    Using Datatables in Spock

--- src/test/groovy/FirstSpecification.groovy
+++ src/test/groovy/FirstSpecification.groovy
@@ -24,4 +24,27 @@ class FirstSpecification extends Specification{
         then:
             list == [2, 3, 4]
     }
+
+    def "Should get an index out of bounds when removing a nonexistent item"() {
+        given:
+            def list = [1, 2, 3, 4]
+
+        when:
+            list.remove(20)
+
+        then:
+            thrown(IndexOutOfBoundsException)
+            list.size() == 4
+    }
+
+    def "numbers to the power of two"(int a, int b, int c) {
+        expect:
+            Math.pow(a, b) == c
+
+        where:
+            a | b | c
+            1 | 2 | 1
+            2 | 2 | 4
+            3 | 2 | 9
+    }
 }

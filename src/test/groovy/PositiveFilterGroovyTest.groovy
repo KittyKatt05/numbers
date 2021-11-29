d306f20d5ec6a13ad11f2e93dee84292f69add16
Catarina <1201338@isep.ipp.pt>
2021-11-29 13:03:50 +0000

    Converted the ListPositiveFilterTest class to Groovy

--- src/test/groovy/PositiveFilterGroovyTest.groovy
+++ src/test/groovy/PositiveFilterGroovyTest.groovy
@@ -1,2 +1,20 @@
-class PositiveFilterGroovyTest {
+import com.aor.numbers.PositiveFilter
+import spock.lang.Specification
+
+class PositiveFilterGroovyTest extends Specification{
+
+    def "accept"() {
+        given:
+            def number = 5
+            def numberN = -5
+
+        when:
+            def psf = new PositiveFilter()
+            def result = psf.accept(number)
+            def resultN = psf.accept(numberN)
+
+        then:
+            result == true
+            resultN == false
+    }
 }

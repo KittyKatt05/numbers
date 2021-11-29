b6ba17b375350b22da6be0b179ca941d1e0db6c6
Catarina <1201338@isep.ipp.pt>
2021-11-29 13:07:15 +0000

    Converted the ListDivisibleByFilterTest class to Groovy

--- src/test/groovy/DivisibleByFilterGroovyTest.groovy
+++ src/test/groovy/DivisibleByFilterGroovyTest.groovy
@@ -1,2 +1,21 @@
-class DivisibleByFilterGroovyTest {
+import com.aor.numbers.DivisibleByFilter
+import spock.lang.Specification
+
+class DivisibleByFilterGroovyTest extends Specification{
+
+    def "accept"() {
+        given:
+            def div = 3
+            def number = 12
+            def number1 = 10
+
+        when:
+            def dbf = new DivisibleByFilter(div)
+            def result = dbf.accept(number);
+            def result1 = dbf.accept(number1)
+
+        then:
+            result == true
+            result1 == false
+    }
 }

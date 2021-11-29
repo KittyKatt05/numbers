9e0325d7388975efe0365c25c6b4e74c84d37477
Catarina <1201338@isep.ipp.pt>
2021-11-29 12:59:15 +0000

    Converted the ListSorterTest class to groovy

--- src/test/groovy/ListSorterGroovyTest.groovy
+++ src/test/groovy/ListSorterGroovyTest.groovy
@@ -1,2 +1,18 @@
-class ListSorterGroovyTest {
+import com.aor.numbers.ListSorter
+import spock.lang.Specification
+
+class ListSorterGroovyTest extends Specification{
+
+    def "sort"(){
+        given:
+            def list = [1, 2, 4, 2]
+
+        when:
+            def sorter = new ListSorter()
+            def sorted = sorter.sort(list)
+            def expected = [1, 2, 2, 4]
+
+        then:
+            sorted == expected
+    }
 }
